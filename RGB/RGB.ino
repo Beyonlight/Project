83e4a8571ef34f6dedf978e5b0e9143729866adb
Corentin Mathé--Deletang <corentin.mathe@cri-paris.org>
2017-01-20 12:20:03 +0100

    RGB sensor code

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -27,31 +27,39 @@
       // Setting red filtered photodiodes to be read
       digitalWrite(S2,LOW);
       digitalWrite(S3,LOW);
+      
       // Reading the output frequency
       frequency = pulseIn(sensorOut, LOW);
+      
       // Printing the value on the serial monitor
       Serial.print("R= ");//printing name
       Serial.print(frequency);//printing RED color frequency
       Serial.print("  ");
-      delay(100);
+      delay(10);
+      
       // Setting Green filtered photodiodes to be read
       digitalWrite(S2,HIGH);
       digitalWrite(S3,HIGH);
+      
       // Reading the output frequency
       frequency = pulseIn(sensorOut, LOW);
+      
       // Printing the value on the serial monitor
       Serial.print("G= ");//printing name
       Serial.print(frequency);//printing RED color frequency
       Serial.print("  ");
-      delay(100);
+      delay(10);
+      
       // Setting Blue filtered photodiodes to be read
       digitalWrite(S2,LOW);
       digitalWrite(S3,HIGH);
+      
       // Reading the output frequency
       frequency = pulseIn(sensorOut, LOW);
+      
       // Printing the value on the serial monitor
       Serial.print("B= ");//printing name
       Serial.print(frequency);//printing RED color frequency
       Serial.println("  ");
-      delay(1000);
+      delay(10);
     }
